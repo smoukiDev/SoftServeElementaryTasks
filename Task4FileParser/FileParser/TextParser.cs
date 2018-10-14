ae2a7724f34af80bcefea3db09bb833975f4c87d
smoukiDev <smakfactory@gmail.com>
2018-10-14 23:50:12 +0300

    Added abstact TextParser class

---
+++ Task4FileParser/FileParser/TextParser.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="TextParser.cs" company="Serhii Maksymchuk">
+// Copyright (c) 2018 by Serhii Maksymchuk. All Rights Reserved.
+// </copyright>
+
+namespace FileParser
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    abstract class TextParser
+    {
+        private string searchValue;
+        private string replaceValue;
+
+        public string SearchValue { get; set; }
+
+        public string ReplaceValue { get; set; }
+
+        public abstract int CountEnteries();
+
+        public abstract int ReplaceEnteries();
+    }
+}
