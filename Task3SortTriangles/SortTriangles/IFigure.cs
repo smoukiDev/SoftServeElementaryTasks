253cb214c5a392b00275877c8486e813c545c82d
smoukiDev <smakfactory@gmail.com>
2018-10-17 10:08:42 +0300

    Added IFigure interface

---
+++ Task3SortTriangles/SortTriangles/IFigure.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="IFigure.cs" company="Serhii Maksymchuk">
+// Copyright (c) 2018 by Serhii Maksymchuk. All Rights Reserved.
+// </copyright>
+
+namespace SortTriangles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IFigure
+    {
+        double CalculatePerimeter();
+
+        double CalculateSquere();
+    }
+}
