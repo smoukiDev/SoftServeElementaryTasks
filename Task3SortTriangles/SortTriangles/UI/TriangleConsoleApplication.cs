b54839d1f5ca53b4569bdf1127205817c211953e
smoukiDev <smakfactory@gmail.com>
2018-10-19 02:58:04 +0300

    Added Validate Method

--- Task3SortTriangles/SortTriangles/UI/TriangleConsoleApplication.cs
+++ Task3SortTriangles/SortTriangles/UI/TriangleConsoleApplication.cs
@@ -1,12 +1,109 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SortTriangles
+﻿namespace SortTriangles
 {
-    class TriangleConsoleApplication
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using SortTriangles.UI;
+    using System.Text.RegularExpressions;
+
+    class TriangleConsoleApplication : IUserGuide
     {
+        private static readonly string SEPARATE_LINE = new string('=', 70);
+        private const int NUMBER_OF_ARGS = 4;
+        private const string ARGUMENT_EXCEPTION_MESSAGE = "Incorrect print format";
+
+        public void Run()
+        {
+            this.DisplayHelpMessage();
+            this.StartUp();
+
+            string key = null;
+            do
+            {
+                var triangleArgs = this.ValidateInput(this.GetInput());
+                Console.WriteLine($"{triangleArgs.name} {triangleArgs.sideA} {triangleArgs.sideB} {triangleArgs.sideC}");
+                Console.WriteLine("Do you want to add triangle?");
+                Console.WriteLine("Print Y or YES to confirm...");
+                Console.WriteLine("Print other string to show list of triangles...");
+                key = Console.ReadLine().ToUpper();
+            }
+            while (key == "Y" || key == "YES");
+        }
+
+        private void StartUp()
+        {
+            Console.Write("Press Enter To Start");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        private string GetInput()
+        {
+            Console.WriteLine("Print triangle parameters, please...");
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">Incorrect print format</exception>
+        private(string name, double sideA, double sideB, double sideC) ValidateInput(string input)
+        {
+            double sideA;
+            double sideB;
+            double sideC;
+            string name;
+
+            input = Regex.Replace(input, " ", string.Empty);
+            input = Regex.Replace(input, "\t", string.Empty);
+
+            string[] parameters = input.Split(',');
+
+            if (parameters.Length != NUMBER_OF_ARGS)
+            {
+                throw new ArgumentException(ARGUMENT_EXCEPTION_MESSAGE);
+            }
+
+            if (parameters[0] == string.Empty)
+            {
+                throw new ArgumentException(ARGUMENT_EXCEPTION_MESSAGE);
+            }
+            else
+            {
+                name = parameters[0];
+            }
+
+            // TODO: Mention exceptions in my documentation
+            try
+            {
+                // TODO: Encoding cases
+                sideA = double.Parse(parameters[1].Replace('.', ','));
+                sideB = double.Parse(parameters[2].Replace('.', ','));
+                sideC = double.Parse(parameters[3].Replace('.', ','));
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException(ARGUMENT_EXCEPTION_MESSAGE);
+            }
+
+            return (name, sideA, sideB, sideC);
+        }
+
+        public void DisplayHelpMessage()
+        {
+            Console.WriteLine(SEPARATE_LINE);
+            Console.WriteLine("Welcome to TriangleSort Application");
+            Console.WriteLine(SEPARATE_LINE);
+            Console.WriteLine("Print format to add triangle:");
+            Console.WriteLine("[name], [side 1], [side 2], [side 3]");
+            Console.WriteLine("Print Y or YES to continue and add one more triangle");
+            Console.WriteLine("Print other string to show list of triangles");
+            Console.WriteLine("Triangles will be diplayed in discending order by square");
+            Console.WriteLine(SEPARATE_LINE);
+        }
     }
 }
