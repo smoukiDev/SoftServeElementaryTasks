0e90f4b2d0a5375f64a072852d8e01c90fe5c6cd
smoukiDev <smakfactory@gmail.com>
2018-10-19 02:43:03 +0300

    Documented Program.cs

--- Task3SortTriangles/SortTriangles/Program.cs
+++ Task3SortTriangles/SortTriangles/Program.cs
@@ -1,38 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="Program.cs" company="Serhii Maksymchuk">
+// Copyright (c) 2018 by Serhii Maksymchuk. All Rights Reserved.
+// </copyright>
 
 namespace SortTriangles
 {
-    class Program
+    /// <summary>
+    /// Contains entery point to program
+    /// </summary>
+    internal class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Method is entery point to program
+        /// </summary>
+        /// <param name="args">String massive of input arguments</param>
+        private static void Main(string[] args)
         {
-            try
-            {
-                Triangle a = new Triangle("tini", null, 3.0, 4.0, 5.0);
-                Console.WriteLine(a.IsExist());
-                Console.WriteLine(a.Name);
-                Console.WriteLine(a[0]);
-                Console.WriteLine(a[1]);
-                Console.WriteLine(a[2]);
-                Console.WriteLine(a[3]);
-                Console.WriteLine(a.CalculatePerimeter());
-                Console.WriteLine(a.CalculateSquare());
-                Console.WriteLine(a.ToString());
-            }
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            catch (IndexOutOfRangeException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            Console.ReadLine();
+            TriangleConsoleApplication app = new TriangleConsoleApplication();
+            app.Run();
         }
     }
 }
