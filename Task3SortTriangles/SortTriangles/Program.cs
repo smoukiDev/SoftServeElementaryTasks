3d913f6fb8f7cc79c72b1db8258b1fe05bf2a199
smoukiDev <smakfactory@gmail.com>
2018-10-18 18:48:05 +0300

    Triangle class complete, documented

--- Task3SortTriangles/SortTriangles/Program.cs
+++ Task3SortTriangles/SortTriangles/Program.cs
@@ -12,17 +12,22 @@ namespace SortTriangles
         {
             try
             {
-                Triangle a = new Triangle("tini", 3.0, 4.0, 5.0);
+                Triangle a = new Triangle("tini", null, 3.0, 4.0, 5.0);
                 Console.WriteLine(a.IsExist());
                 Console.WriteLine(a.Name);
                 Console.WriteLine(a[0]);
                 Console.WriteLine(a[1]);
                 Console.WriteLine(a[2]);
+                Console.WriteLine(a[3]);
                 Console.WriteLine(a.CalculatePerimeter());
                 Console.WriteLine(a.CalculateSquare());
                 Console.WriteLine(a.ToString());
             }
-            catch (TriangleNoExistException ex)
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (IndexOutOfRangeException ex)
             {
                 Console.WriteLine(ex.Message);
             }
--- Task3SortTriangles/SortTriangles/Triangle.cs
+++ Task3SortTriangles/SortTriangles/Triangle.cs
@@ -1,22 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="Triangle.cs" company="Serhii Maksymchuk">
+// Copyright (c) 2018 by Serhii Maksymchuk. All Rights Reserved.
+// </copyright>
 
 namespace SortTriangles
 {
-    public class Triangle : IFigure
+    using System;
+
+    /// <summary>
+    /// Reprecents Triangle with name and measure unit
+    /// </summary>
+    public class Triangle : IFigure, IComparable<Triangle>
     {
-        private static readonly string MEASURE = "sm";
+        private readonly double _sideA;
+        private readonly double _sideB;
+        private readonly double _sideC;
 
-        private double _sideA;
-        private double _sideB;
-        private double _sideC;
-
-        public string Name { get; set; }
-
-        public Triangle(string name, double sideA, double sideB, double sideC)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Triangle"/> class.
+        /// </summary>
+        /// <param name="name">Triangle name</param>
+        /// <param name="measure">Measure unit</param>
+        /// <param name="sideA">Side A of triangle</param>
+        /// <param name="sideB">Side B of triangle</param>
+        /// <param name="sideC">Side C of triangle</param>
+        public Triangle(string name, string measure, double sideA, double sideB, double sideC)
         {
             if (name == null)
             {
@@ -27,7 +34,7 @@ namespace SortTriangles
                 Name = name;
             }
 
-            if (sideA <=0)
+            if (sideA <= 0)
             {
                 _sideA = 0;
             }
@@ -53,29 +60,30 @@ namespace SortTriangles
             {
                 _sideC = sideC;
             }
+
+            this.Measure = measure ?? "sm";
         }
 
-        public bool IsExist()
-        {
-            if (_sideB + _sideC > _sideA)
-            {
-                if (_sideA + _sideC > _sideB)
-                {
-                    if (_sideA + _sideB > _sideC)
-                    {
-                        return true;
-                    }
-                }
-            }
+        /// <summary>
+        /// Gets or sets measure unit
+        /// </summary>
+        public string Measure { get; set; }
 
-            return false;
-        }
+        /// <summary>
+        /// Gets or sets triangle name
+        /// </summary>
+        public string Name { get; set; }
 
+        /// <summary>
+        /// Gets a side of triangle by indexes 0,1,2
+        /// </summary>
+        /// <param name="index">Index of side</param>
+        /// <returns>Side of triangle</returns>
         public double this[int index]
         {
             get
             {
-                if(index == 0)
+                if (index == 0)
                 {
                     return _sideA;
                 }
@@ -94,6 +102,30 @@ namespace SortTriangles
             }
         }
 
+        /// <summary>
+        /// Indicates whether trianles exists with such sides
+        /// </summary>
+        /// <returns>True if triangle exist, false if it doesn't exist</returns>
+        public bool IsExist()
+        {
+            if (_sideB + _sideC > _sideA)
+            {
+                if (_sideA + _sideC > _sideB)
+                {
+                    if (_sideA + _sideB > _sideC)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Calculates a perimeter of the trianle
+        /// </summary>
+        /// <returns>Perimeter of triangle</returns>
         public double CalculatePerimeter()
         {
             if (this.IsExist())
@@ -107,16 +139,47 @@ namespace SortTriangles
             throw new ArgumentException(message);
         }
 
+        /// <summary>
+        /// Calculates a square of the trianle
+        /// </summary>
+        /// <returns>Square of triangle</returns>
         public double CalculateSquare()
         {
             double half = this.CalculatePerimeter() / 2;
             return Math.Sqrt(half * (half - _sideA) * (half - _sideB) * (half - _sideC));
         }
 
+        /// <summary>
+        /// Returns information about name, square, measure of the triangle 
+        /// </summary>
+        /// <returns>String with information about name, square, measure</returns>
         public override string ToString()
         {
             return $"[Triangle {this.Name}]: "
-                 + $"{this.CalculateSquare()} {MEASURE}";
+                 + $"{this.CalculateSquare()} {this.Measure}";
+        }
+
+        /// <summary>
+        /// Compares this instance of <see cref="Triangle"/> with other
+        /// </summary>
+        /// <param name="other"> Other instance of <see cref="Triangle"/> to compare with </param>
+        /// <returns> Comparison result: 1 - this smaller than other, -1 - larger, 0 - equal </returns>
+        public int CompareTo(Triangle other)
+        {
+            double thisSquare = this.CalculateSquare();
+            double otherSquare = other.CalculateSquare();
+            if (thisSquare > otherSquare)
+            {
+                return -1;
+            }
+            else if (thisSquare < otherSquare)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
