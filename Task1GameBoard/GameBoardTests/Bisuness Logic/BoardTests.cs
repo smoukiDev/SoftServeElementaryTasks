2d385cdfdd8f78d7bd80f7ef9a80410aeea0a692
smoukiDev <smakfactory@gmail.com>
2018-10-20 17:48:55 +0300

    Add unit tests template

---
+++ Task1GameBoard/GameBoardTests/Bisuness Logic/BoardTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameBoard.Bisuness_Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBoard.Bisuness_Logic.Tests
+{
+    [TestClass]
+    public class BoardTests
+    {
+        [TestMethod]
+        public void ResetBoardTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void BuildBoardTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void BoardTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
