1a0b2b70b34854c9c00f9ad978e85d3deca15f30
smoukiDev <smakfactory@gmail.com>
2018-10-20 13:18:23 +0300

    Add class cell

---
+++ Task1GameBoard/GameBoard/Bisuness Logic/Cell.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="Cell.cs" company="Serhii Maksymchuk">
+// Copyright (c) 2018 by Serhii Maksymchuk. All Rights Reserved.
+// </copyright>
+
+namespace GameBoard.Bisuness_Logic
+{
+    /// <summary>
+    /// Represents board cell
+    /// </summary>
+    public class Cell : ICell
+    {
+        /// <summary>
+        /// Gets or sets board cell color
+        /// </summary>
+        public CellColor Color { get; set; }
+
+        /// <summary>
+        /// Get name of color for board cell
+        /// </summary>
+        /// <returns>Name of color</returns>
+        public override string ToString()
+        {
+            return this.Color.ToString();
+        }
+    }
+}
