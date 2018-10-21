b706e11c97d32fdf02ff0a50c5aeca73a0a0e3a9
smoukiDev <smakfactory@gmail.com>
2018-10-22 01:02:17 +0300

    Recreated Library project

---
+++ Task7_8Sequence/SequenceLibrary/NaturalSquareSequence.cs
@@ -0,0 +1,64 @@
+﻿// <copyright file="NaturalSquareSequence.cs" company="Serhii Maksymchuk">
+// Copyright (c) 2018 by Serhii Maksymchuk. All Rights Reserved.
+// </copyright>
+
+namespace SequencesLib
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Reprecents sequence of natural numbers with square less then N^2
+    /// </summary>
+    public class NaturalSquareSequence : Sequence<int>
+    {
+        private const int DOWN_LIMIT = 1;
+
+        private NaturalSquareSequence(int squareLimit)
+            : base(DOWN_LIMIT, squareLimit)
+        {
+        }
+
+        /// <summary>
+        /// Gets bound of sequence
+        /// </summary>
+        public int SquareLimit
+        {
+            get
+            {
+                return this.UpLimit;
+            }
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="NaturalSquareSequence"/> class.
+        /// </summary>
+        /// <param name="squareLimit">Square of number limit</param>
+        /// <returns>Instance of <see cref="NaturalSquareSequence"/></returns>
+        /// <exception cref="ArgumentException">Square Limit should be greater than zero</exception>
+        public static NaturalSquareSequence Create(int squareLimit)
+        {
+            if (squareLimit <= 0)
+            {
+                throw new ArgumentException("Square Limit should be greater than zero");
+            }
+
+            return new NaturalSquareSequence(squareLimit);
+        }
+
+        /// <summary>
+        /// Iterator return natural numbers with square less then SquareLimit
+        /// </summary>
+        /// <returns>Sequence elements</returns>
+        public override IEnumerable<int> GetSequence()
+        {
+            for (int counter = this.DownLimit; counter < this.UpLimit; counter++)
+            {
+                if (counter * counter < this.UpLimit)
+                {
+                    yield return counter;
+                }
+            }
+        }
+    }
+}
---
+++ Task7_8Sequence/SequenceLibrary/Sequence{T}.cs
@@ -0,0 +1,68 @@
+﻿// <copyright file="Sequence{T}.cs" company="Serhii Maksymchuk">
+// Copyright (c) 2018 by Serhii Maksymchuk. All Rights Reserved.
+// </copyright>
+
+namespace SequencesLib
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Provides foundation to build sequences
+    /// </summary>
+    /// <typeparam name="T">Type of sequence elements</typeparam>
+    public abstract class Sequence<T>
+    {
+        private T downLimit;
+        private T upLimit;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Sequence{T}"/> class.
+        /// </summary>
+        /// <param name="downLimit">Sequence down bound of range</param>
+        /// <param name="upLimit">Sequence up bound of range</param>
+        public Sequence(T downLimit, T upLimit)
+        {
+            this.downLimit = downLimit;
+            this.upLimit = upLimit;
+        }
+
+        /// <summary>
+        /// Gets or sets sequence down bound of range
+        /// </summary>
+        protected T DownLimit
+        {
+            get
+            {
+                return this.downLimit;
+            }
+
+            set
+            {
+                this.downLimit = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets sequence up bound of range
+        /// </summary>
+        protected T UpLimit
+        {
+            get
+            {
+                return this.upLimit;
+            }
+
+            set
+            {
+                this.upLimit = value;
+            }
+        }
+
+        /// <summary>
+        /// Method Iterator for selection element
+        /// of sequence by some condition and logic
+        /// </summary>
+        /// <returns>Sequence elements</returns>
+        public abstract IEnumerable<T> GetSequence();
+    }
+}
