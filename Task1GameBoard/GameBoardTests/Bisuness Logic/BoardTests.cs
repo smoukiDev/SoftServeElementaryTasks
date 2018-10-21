3bb071611247a55624447f55394e183b8baf1eb2
smoukiDev <smakfactory@gmail.com>
2018-10-21 15:21:26 +0300

    Added all unit tests

--- Task1GameBoard/GameBoardTests/Bisuness Logic/BoardTests.cs
+++ Task1GameBoard/GameBoardTests/Bisuness Logic/BoardTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace GameBoard.Bisuness_Logic.Tests
 {
@@ -16,7 +17,7 @@ namespace GameBoard.Bisuness_Logic.Tests
         [DataRow(-8, 8)]
         [DataRow(8, -8)]
         [DataRow(-8, -8)]
-        public void ResetBoardTest_ThrowsArgumentException(int width, int height)
+        public void ResetBoard_ThrowsArgumentException(int width, int height)
         {
             // Arrange
             Board board = new Board();
@@ -47,7 +48,7 @@ namespace GameBoard.Bisuness_Logic.Tests
         }
 
         [TestMethod]
-        public void BuildBoardTest_FirstCellDark()
+        public void BuildBoard_FirstCellDark()
         {
             // Arrange
             int width = 4;
@@ -59,45 +60,43 @@ namespace GameBoard.Bisuness_Logic.Tests
 
             #region Expected BoardSurface Initialization
 
-            Cell[,] expectedBoardSurface = new Cell[width, height];
+            Cell[,] expectedBoardSurface = new Cell[height, width];
+            Cell cellOne = new Cell (CellColor.Black);
+            Cell cellTwo = new Cell (CellColor.White);
             // Row One of expected board
-            expectedBoardSurface[0, 0] = new Cell(CellColor.Black);
-            expectedBoardSurface[0, 1] = new Cell(CellColor.White);
-            expectedBoardSurface[0, 2] = new Cell(CellColor.Black);
-            expectedBoardSurface[0, 3] = new Cell(CellColor.White);
+            expectedBoardSurface[0, 0] = cellOne;
+            expectedBoardSurface[0, 1] = cellTwo;
+            expectedBoardSurface[0, 2] = cellOne;
+            expectedBoardSurface[0, 3] = cellTwo;
             // Row Two of expected board
-            expectedBoardSurface[1, 0] = new Cell(CellColor.White);
-            expectedBoardSurface[1, 1] = new Cell(CellColor.Black);
-            expectedBoardSurface[1, 2] = new Cell(CellColor.White);
-            expectedBoardSurface[1, 3] = new Cell(CellColor.Black);
+            expectedBoardSurface[1, 0] = cellTwo;
+            expectedBoardSurface[1, 1] = cellOne;
+            expectedBoardSurface[1, 2] = cellTwo;
+            expectedBoardSurface[1, 3] = cellOne;
             // Row Three of expected board
-            expectedBoardSurface[2, 0] = new Cell(CellColor.Black);
-            expectedBoardSurface[2, 1] = new Cell(CellColor.White);
-            expectedBoardSurface[2, 2] = new Cell(CellColor.Black);
-            expectedBoardSurface[2, 3] = new Cell(CellColor.White);
+            expectedBoardSurface[2, 0] = cellOne;
+            expectedBoardSurface[2, 1] = cellTwo;
+            expectedBoardSurface[2, 2] = cellOne;
+            expectedBoardSurface[2, 3] = cellTwo;
             // Row Four of expected board
-            expectedBoardSurface[3, 0] = new Cell(CellColor.White);
-            expectedBoardSurface[3, 1] = new Cell(CellColor.Black);
-            expectedBoardSurface[3, 2] = new Cell(CellColor.White);
-            expectedBoardSurface[3, 3] = new Cell(CellColor.Black);
+            expectedBoardSurface[3, 0] = cellTwo;
+            expectedBoardSurface[3, 1] = cellOne;
+            expectedBoardSurface[3, 2] = cellTwo;
+            expectedBoardSurface[3, 3] = cellOne;
 
             #endregion
 
-            bool actual = false;
-            bool expected = true;
+            int actual = 0;
+            int expected = height*width;
 
             // Act
-            for (int heightIndex = 0; height < board.Height; height++)
+            for (int heightIndex = 0; heightIndex < board.Height; heightIndex++)
             {
                 for (int widthIndex = 0; widthIndex < board.Width; widthIndex++)
                 {
-                    if(actualBoardSurface[heightIndex, widthIndex].Color == expectedBoardSurface[heightIndex, widthIndex].Color)
+                    if((int)actualBoardSurface[heightIndex, widthIndex].Color == (int)expectedBoardSurface[heightIndex, widthIndex].Color)
                     {
-                        actual = true;
-                    }
-                    else
-                    {
-                        actual = false;
+                        actual++;
                     }
                 }
             }
@@ -107,9 +106,141 @@ namespace GameBoard.Bisuness_Logic.Tests
         }
 
         [TestMethod]
-        public void BoardTest()
+        public void BuildBoard_FirstCellLight()
         {
-            Assert.Fail();
+            // Arrange
+            int width = 4;
+            int height = 4;
+            bool isFirstCellWhite = true;
+            Board board = new Board(width, height, isFirstCellWhite);
+            board.BuildBoard();
+            Cell[,] actualBoardSurface = board.BoardSurface;
+
+            #region Expected BoardSurface Initialization
+
+            Cell[,] expectedBoardSurface = new Cell[height, width];
+            Cell cellOne = new Cell(CellColor.White);
+            Cell cellTwo = new Cell(CellColor.Black);
+            // Row One of expected board
+            expectedBoardSurface[0, 0] = cellOne;
+            expectedBoardSurface[0, 1] = cellTwo;
+            expectedBoardSurface[0, 2] = cellOne;
+            expectedBoardSurface[0, 3] = cellTwo;
+            // Row Two of expected board
+            expectedBoardSurface[1, 0] = cellTwo;
+            expectedBoardSurface[1, 1] = cellOne;
+            expectedBoardSurface[1, 2] = cellTwo;
+            expectedBoardSurface[1, 3] = cellOne;
+            // Row Three of expected board
+            expectedBoardSurface[2, 0] = cellOne;
+            expectedBoardSurface[2, 1] = cellTwo;
+            expectedBoardSurface[2, 2] = cellOne;
+            expectedBoardSurface[2, 3] = cellTwo;
+            // Row Four of expected board
+            expectedBoardSurface[3, 0] = cellTwo;
+            expectedBoardSurface[3, 1] = cellOne;
+            expectedBoardSurface[3, 2] = cellTwo;
+            expectedBoardSurface[3, 3] = cellOne;
+
+            #endregion
+
+            int actual = 0;
+            int expected = height * width;
+
+            // Act
+            for (int heightIndex = 0; heightIndex < board.Height; heightIndex++)
+            {
+                for (int widthIndex = 0; widthIndex < board.Width; widthIndex++)
+                {
+                    if ((int)actualBoardSurface[heightIndex, widthIndex].Color == (int)expectedBoardSurface[heightIndex, widthIndex].Color)
+                    {
+                        actual++;
+                    }
+                }
+            }
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(0, 0, true)]
+        [DataRow(-8, 8, false)]
+        [DataRow(8, -8, true)]
+        [DataRow(-8, -8, false)]
+        public void BuildBoard_ThrowsArgumentException(int width, int height, bool isWhiteCell)
+        {
+            // Arrange
+            Board board = new Board(width, height, isWhiteCell);
+            // Act
+
+            // Arrange
+            Assert.ThrowsException<ArgumentException>(() => board.BuildBoard());
+        }
+
+        [TestMethod]
+        [DataRow(4, 4, 0, 1)]
+        [DataRow(4, 4, 1, 0)]
+        [DataRow(4, 4, 0, 0)]
+        [DataRow(4, 4, 1, 5)]
+        [DataRow(4, 4, 5, 1)]
+        [DataRow(4, 4, 5, 5)]
+        [DataRow(4, 4, -1, -1)]
+        public void Indexer_ThrowsIndexOutRangeException(int width, int height, int indexWidth, int indexHeight)
+        {
+            // Arrange
+            bool isWhiteCell = true;
+            Board board = new Board(width, height, isWhiteCell);
+
+            // Act
+            board.BuildBoard();
+
+            // Arrange
+            Assert.ThrowsException<IndexOutOfRangeException>(() => board[indexHeight, indexWidth]);
+        }
+
+        [TestMethod]
+        [DataRow(4, 4, 1, 1)]
+        [DataRow(4, 4, 4, 4)]
+        [DataRow(4, 4, 3, 2)]
+        [DataRow(4, 4, 2, 1)]
+        public void Indexer_ThrowsArgumentNullException(int width, int height, int indexWidth, int indexHeight)
+        {
+            // Arrange
+            bool isWhiteCell = true;
+            Board board = new Board(width, height, isWhiteCell);
+
+            // Act
+            // Don't proceed
+            // board.BuildBoard();
+
+            // Arrange
+            Assert.ThrowsException<ArgumentNullException>(() => board[indexHeight, indexWidth]);
+        }
+
+        [TestMethod]
+        [DataRow(4, 4, 1, 1)]
+        [DataRow(4, 4, 4, 4)]
+        [DataRow(4, 4, 3, 2)]
+        [DataRow(4, 4, 2, 1)]
+        public void Indexer_ReturnCellSuccessfully(int width, int height, int indexWidth, int indexHeight)
+        {
+            // Arrange
+            bool isWhiteCell = true;
+            Board board = new Board(width, height, isWhiteCell);
+
+            // Act
+            board.BuildBoard();
+
+            // Arrange
+            try
+            {
+                Cell cell = (Cell)board[indexHeight, indexWidth];
+            }
+            catch(Exception)
+            {
+                Assert.Fail();
+            }
         }
     }
 }
