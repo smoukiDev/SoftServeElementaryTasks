aa46bbc1da4ff005d5bcbb7683d2181bd3adc072
smoukiDev <smakfactory@gmail.com>
2018-10-22 20:36:42 +0300

    Added Interface IParse

---
+++ Task4FileParser/FileParser/IParse.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="IParse.cs" company="Serhii Maksymchuk">
+// Copyright (c) 2018 by Serhii Maksymchuk. All Rights Reserved.
+// </copyright>
+
+namespace FileParser
+{
+    /// <summary>
+    /// Provides prototype of method to parse  strings
+    /// </summary>
+    public interface IParse
+    {
+        /// <summary>
+        /// Perform parsing of string
+        /// </summary>
+        /// <returns>
+        /// Number ofSuccess parsing manipulations
+        /// </returns>
+        int Parse();
+    }
+}
