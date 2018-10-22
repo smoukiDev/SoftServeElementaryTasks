babc270a34fd3a7f73f53d977931084aeb7640a9
smoukiDev <smakfactory@gmail.com>
2018-10-23 00:00:52 +0300

    Added foundation for UnitTests with files proccesing

---
+++ Task4FileParser/FileParserTests/Business Logic/TextFileMatchParserTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileParser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileParser.Tests
+{
+    [TestClass]
+    public class TextFileMatchParserTests
+    {
+        [TestMethod]
+        [DeploymentItem(@"TestFiles\FileForParserTests.txt")]
+        [DataRow("Sample", 9)]
+        public void ParseTest_ReturnMatchesCorrectly(string searchValue, int matches)
+        {
+            // Arrange
+            TextFileMatchParser parser = new TextFileMatchParser("FileForParserTests.txt", searchValue);
+            int expected = matches;
+            int actual;
+
+            // Act
+            actual = parser.Parse();
+            parser.Dispose();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Task4FileParser/FileParserTests/Business Logic/TextFileReplacementParserTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileParser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace FileParser.Tests
+{
+    [TestClass]
+    public class TextFileReplacementParserTests
+    {
+        [TestMethod]
+        [DeploymentItem(@"TestFiles\FileForParserTests.txt")]
+        [DataRow("Sample","Examples", 9)]
+        public void ParseTest_ReturnMatchesCorrectly(string searchValue, string replaceValue, int matches)
+        {
+            // Arrange
+            Debug.WriteLine(Directory.GetCurrentDirectory());
+
+            string targetFilePath = Directory.GetCurrentDirectory() + @"\TestFiles\FileForParserTests.txt";
+            
+            TextFileReplacementParser parser = new TextFileReplacementParser(targetFilePath, searchValue, replaceValue);
+            int expected = matches;
+            int actual;
+
+            // Act
+            actual = parser.Parse();
+            File.Delete(parser.FileCopy);
+            parser.Dispose();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
