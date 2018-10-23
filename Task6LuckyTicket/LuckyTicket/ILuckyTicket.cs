761bf281e1a26bd44b50d1640b85411db89ea824
smoukiDev <smakfactory@gmail.com>
2018-10-23 15:21:32 +0300

    Added interface ILucky

---
+++ Task6LuckyTicket/LuckyTicket/ILuckyTicket.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="ILuckyTicket.cs" company="Serhii Maksymchuk">
+// Copyright (c) 2018 by Serhii Maksymchuk. All Rights Reserved.
+// </copyright>
+
+namespace LuckyTicket
+{
+    /// <summary>
+    /// Provides method IsLucky to indicate lucky ticket
+    /// using some alghorithm
+    /// </summary>
+    public interface ILuckyTicket
+    {
+        /// <summary>
+        /// Prototype of method, which indicates wheter Ticket is lucky
+        /// </summary>
+        /// <param name="ticket">TTicket</param>
+        /// <returns>Result true - for lucky tickets, false - if not</returns>
+        bool IsLucky(Ticket ticket);
+    }
+}
